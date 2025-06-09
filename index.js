b1314ce32d62b108924726ab2e60c1a49eec475e
Sohom Roy <110277599+sohom2004@users.noreply.github.com>
2025-06-10 02:34:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
