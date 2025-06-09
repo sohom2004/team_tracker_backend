9b840ff7c4861185ecf73599353d12482ef48c2b
Sohom Roy <110277599+sohom2004@users.noreply.github.com>
2025-06-09 23:28:05 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
  const db = mysql.createConnection({
-   host: process.env.DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB
 });
 
 db.connect((err) => {
