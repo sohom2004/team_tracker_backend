ab9e49c254b72697413fabf338485dc1ffd5bd45
Sohom Roy <110277599+sohom2004@users.noreply.github.com>
2025-06-10 12:40:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const authRoutes = require('./routes/authRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://team-tracker-frontend-two.vercel.app", // your live frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
