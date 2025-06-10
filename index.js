0475b2efcd07903bd6342dc576f374e9341871a8
Sohom Roy <110277599+sohom2004@users.noreply.github.com>
2025-06-10 12:44:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log('Server running on port 5000');
 });
